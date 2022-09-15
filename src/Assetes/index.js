63e9644a7b7806c44034b3716f6ef230d3ce994d
Asad_khan <skan7048@gmail.com>
2022-09-15 16:09:04 +0500

    Completed Onboarding Screen's

--- src/Assetes/index.js
+++ src/Assetes/index.js
@@ -2,6 +2,7 @@ export default Images = {
   boarding01: require('./onBoarding01.png'),
   boarding02: require('./onBoarding02.png'),
   boarding03: require('./onBoarding03.png'),
+  boarding04: require('./onBoarding04.png'),
   cross: require('./cross.png'),
   facebook: require('./facebook.png'),
   google: require('./google.png'),
